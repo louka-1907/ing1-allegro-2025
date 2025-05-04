b85613b293b50d1802ec880b42eb4ddc8a902f75
Arthurgiraud <arthur.giraud21@gmail.com>
2025-05-04 15:54:02 +0200

    menu h

--- menu.h
+++ menu.h
@@ -1,8 +1,21 @@
-//
-// Created by DELL on 04/05/2025.
-//
-
 #ifndef MENU_H
 #define MENU_H
 
+int afficher_menu();
+
 #endif //MENU_H
+#include <stdio.h>
+#include <allegro.h>
+
+#include "initialisation_allegro.h"
+#include "menu.h"
+
+int main(int argc, char *argv[])
+{
+    initialisation_allegro();
+
+    afficher_menu();
+
+    allegro_exit();
+    return 0;
+}END_OF_MAIN();
